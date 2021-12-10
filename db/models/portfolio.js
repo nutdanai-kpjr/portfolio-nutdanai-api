a0dd6f5f7fdd009a9f5c2954e89a47ce6aa34472
Nutin208 <nutdanai.282@mail.kmutt.ac.th>
2021-12-10 17:56:19 +0700

    [UPDATE] Portfolio Model

--- db/models/portfolio.js
+++ db/models/portfolio.js
@@ -5,8 +5,6 @@ const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
   title: { type: String, required: true, maxlength: 128 },
-  company: { type: String, required: true, maxlength: 64},
-  companyWebsite: { type: String, required: true, maxlength: 128},
   location: { type: String, required: true },
   jobTitle: { type: String, required: true },
   description: { type: String, required: true },
